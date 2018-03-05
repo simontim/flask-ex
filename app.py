6b92413eec85efe5f5d458389489adfd5d6011f7
Simone Mastrodonato <simone.mastrodonato@telecomitalia.it>
2018-03-05 15:03:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-From flask import Flask
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,4 +6,4 @@ def hello_world():
     return 'Flask Dockerized'
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    app.run()
