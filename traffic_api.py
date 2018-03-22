3155cb5c753241bb05518e4d0c90c821abc40f9d
Simone Mastrodonato <simone.mastrodonato@telecomitalia.it>
2018-03-22 16:39:33 +0100

    Update traffic_api.py

--- traffic_api.py
+++ traffic_api.py
@@ -12,7 +12,7 @@ def test_xpath():
     driver = webdriver.Remote(command_executor='http://sel01:4444/wd/hub',desired_capabilities=capabilities)
     driver.get("https://www.google.com/maps/dir/41.9147798,12.3850072/41.8650296,12.6014119")
     km = driver.find_element_by_xpath("//div[@id='pane']")
-    locator2 = "//div[@id="section-directions-trip-0"]/div[2]/div[1]/div[1]/div[1]/span[1]"
+    locator2 = "//div[@id='section-directions-trip-0']/div[2]/div[1]/div[1]/div[1]/span[1]"
     km2 = driver.find_element_by_xpath(locator2)
     print km2
     print km
