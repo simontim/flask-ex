19bc409a9cf8e58e2dbf0b5c42eba67c9d232096
Simone Mastrodonato <simone.mastrodonato@telecomitalia.it>
2018-03-22 16:49:03 +0100

    Update traffic_api.py

--- traffic_api.py
+++ traffic_api.py
@@ -13,8 +13,9 @@ def test_xpath():
     driver.get("https://www.google.com/maps/dir/41.9147798,12.3850072/41.8650296,12.6014119")
     km = driver.find_element_by_xpath("//div[@id='pane']")
     #locator2 = "//div[@id='section-directions-trip-0']"
-    locator2 = "/html/body/jsl/div[3]/div[8]/div[8]/div/div[1]/div/div/div[5]/div[1]/div[2]/div[1]/div[1]/div[1]/span[1]"
-    km2 = driver.find_element_by_xpath(locator2)
+    #locator2 = "/html/body/jsl/div[3]/div[8]/div[8]/div/div[1]/div/div/div[5]/div[1]/div[2]/div[1]/div[1]/div[1]/span[1]"
+    #km2 = driver.find_element_by_xpath(locator2)
+    km2 = driver.find_element_by_xpath("/html/body/jsl/div[3]/div[8]/div[8]/div/div[1]/div/div/div[5]/div[1]/div[2]/div[1]/div[1]/div[1]/span[1]")
     print km2
     print km
     #time.sleep( 10 )
