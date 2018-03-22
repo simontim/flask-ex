074b6f2784f98c27767aa560297de6086d34a987
Simone Mastrodonato <simone.mastrodonato@telecomitalia.it>
2018-03-22 16:06:35 +0100

    Update traffic_api.py

--- traffic_api.py
+++ traffic_api.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.by import By
+import time
 
 def test_xpath():
  
@@ -10,6 +11,8 @@ def test_xpath():
  
     driver = webdriver.Remote(command_executor='http://sel01:4444/wd/hub',desired_capabilities=capabilities)
     driver.get("https://www.google.com/maps/dir/41.9147798,12.3850072/41.8650296,12.6014119/data=!3m1!4b1!4m2!4m1!5i1")
+    time.sleep( 10 )
     driver.save_screenshot('screenshot.png')
+    
     
     driver.close()    
